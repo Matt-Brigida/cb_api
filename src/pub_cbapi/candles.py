b9c7f922dacf1a04f9b1be0ca83e48aebef857fe
Matt Brigida <mdbrigida@gmail.com>
2022-11-26 18:17:45 -0500

    candles working

--- src/pub_cbapi/candles.py
+++ src/pub_cbapi/candles.py
@@ -1,12 +1,19 @@
 import requests
 import pandas as pd
+import ast
+import time
 
-def historical_daily_data(productID, frequency):
+def historical_daily_data(productID):
 
-    url = "https://api.exchange.coinbase.com/products/" + productID + "/candles?granularity=8600"
+    url = "https://api.exchange.coinbase.com/products/" + productID + "/candles?granularity=86400"
 
     headers = {"accept": "application/json"}
 
     response = requests.get(url, headers=headers)
 
-    print(response.text)
+    tmp = pd.DataFrame(ast.literal_eval(response.text), columns=['date', 'open', 'high', 'low', 'close', 'volume'])
+    tmp['date'] = tmp['date'].apply(lambda x: time.strftime("%Y-%m-%d", time.localtime(x)))
+    tmp = tmp.set_index('date')
+    return(tmp)
+
+eth = historical_daily_data("ETH-USD")
